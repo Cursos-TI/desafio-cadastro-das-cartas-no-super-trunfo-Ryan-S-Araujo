d9e21c1473b5ef50d38cbe65453bce1d6ced481b
Ryan da Silva Araújo <ryansilvaaraujo25@gmail.com>
2025-01-23 15:21:56 -0300

    CartasSuperTrunfo.cRyan

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,57 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+typedef struct {
+    char codigo[5];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Cidade;
+
+void cadastrarCidade(Cidade *cidade) {
+    printf("\nDigite o código da cidade:");
+    scanf("%s", cidade->codigo);
+
+    printf("\nDigite a população da cidade:");
+    scanf("%d", &cidade->populacao);
+
+    printf("\nDigite a área da cidade:");
+    scanf("%f", &cidade->area);
+
+    printf("\nDigite o PIB da cidade:");
+    scanf("%f", &cidade->pib);
+
+    printf("\nDigite o número de pontos turísticos da cidade:");
+    scanf("%d", &cidade->pontos_turisticos);
+}
+
+void exibirCidade(Cidade cidade) {
+    
+    printf("\nDados da Cidade %s:\n", cidade.codigo);
+    printf("\nPopulação: %d\n", cidade.populacao);
+    printf("\nÁrea: %.2f km²\n", cidade.area);
+    printf("\nPIB: R$ %.2f bilhões\n", cidade.pib);
+    printf("\nPontos turísticos: %d\n", cidade.pontos_turisticos);
+}
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    Cidade cidades[8][4]; 
+
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    for (char estado = 'A'; estado <= 'H'; estado++) {
+        for (int i = 0; i < 4; i++) {
+            printf("\nCadastro da Cidade %c%02d:\n", estado, i + 1);
+            cadastrarCidade(&cidades[estado - 'A'][i]);
+        }
+    }
+
+
+    printf("\n---- Dados das Cidades Cadastradas ----\n");
+    for (char estado = 'A'; estado <= 'H'; estado++) {
+        for (int i = 0; i < 4; i++) {
+            exibirCidade(cidades[estado - 'A'][i]);
+        }
+    }
 
     return 0;
 }
